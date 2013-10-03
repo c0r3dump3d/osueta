9b322a0e4166a8d6f8f2e32717df80afe0ce8138
c0r3dump3d <coredump@austitici.org>
2013-10-03 15:46:06 +0200

    first commit

---
+++ osueta.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__license__="""
+osueta (OpenSSH User Enumeration Timing Attack)
+
+A simple script to exploit the OpenSSH User Enumeration Timing Attack:
+
+http://cureblog.de/openssh-user-enumeration-time-based-attack/
+http://seclists.org/fulldisclosure/2013/Jul/88 
+
+Authors:
+	c0r3dump | coredump<@>autistici.org
+	rofen | rofen<@>gmx.de
+
+Osueta project site: https://github.com/
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+The authors declines any responsibility in the use of this tool.
+"""
+
+from osumain import main
+
+if __name__=="__main__":
+    main()
